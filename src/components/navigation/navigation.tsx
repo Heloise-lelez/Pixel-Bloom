38cadfdc06a10793b1f6e37e6a3eb265e79652cc
Céline Eap <celine-chhun-huy.eap@edu.devinci.fr>
2025-01-30 14:30:15 +0100

    :construction: WIP header responsive

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,30 +1,80 @@
+'use client'
+
+import {useState} from "react";
 import Link from "next/link";
 import styles from "./navigation.module.css";
 import Image from "next/image";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState("");
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  const openModal = (content: string) => {
+    setModalContent(content);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <>
-      <ul className={styles.nav}>
-        <Image
-          src={"/logo_transparent.png"}
-          alt={"Pixel flower"}
-          height={44}
-          width={58}
-        />
+      <div className={styles.nav}>
         <Link className={styles.title} href="/">
+          <Image
+              src={"/logo_transparent.png"}
+              alt={"Pixel flower"}
+              height={44}
+              width={58}
+          />
           PIXEL BLOOM{" "}
         </Link>
-        <Link className={styles.link} href="/projects">
-          Nos projets
-        </Link>
-        <Link className={styles.link} href="/agency">
-          Notre Agence
-        </Link>
-        <Link className={styles.link} href="/contact">
-          Contactez-nous
-        </Link>
-      </ul>
+
+        <div className={styles.burgerIcon} onClick={toggleMenu}>
+          <div className={isOpen ? styles.burgerOpen : styles.burgerLine}></div>
+          <div className={isOpen ? styles.burgerOpen : styles.burgerLine}></div>
+          <div className={isOpen ? styles.burgerOpen : styles.burgerLine}></div>
+        </div>
+
+        {/* Menu items */}
+        <ul className={styles.menuDesktop}>
+          <li>
+            <Link className={styles.link} href="/projects" onClick={() => openModal('Projects Content')}>
+              Nos projets
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} href="/agency" onClick={() => openModal('Agency Content')}>
+              Notre Agence
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} href="/contact" onClick={() => openModal('Contact Content')}>
+              Contactez-nous
+            </Link>
+          </li>
+        </ul>
+
+        <ul className={isOpen ? styles.menuOpen : styles.menuClosed}>
+          <li>
+            <Link className={styles.link} href="/projects" onClick={() => openModal('Projects Content')}>
+              Nos projets
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} href="/agency" onClick={() => openModal('Agency Content')}>
+              Notre Agence
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} href="/contact" onClick={() => openModal('Contact Content')}>
+              Contactez-nous
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
