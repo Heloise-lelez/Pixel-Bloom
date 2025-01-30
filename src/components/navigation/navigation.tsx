8d20c44292ddde99a4d934c6aa4cd74fe2941eae
Héloïse Le Lez <lelez.heloise@gmail.com>
2025-01-30 16:08:46 +0100

    ♿️ fix nav accessibility

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useState } from "react";
 import Link from "next/link";
@@ -6,30 +6,57 @@ import Image from "next/image";
 import styles from "./navigation.module.css";
 
 const Navigation = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => setIsOpen(!isOpen);
+  const toggleMenu = () => setIsOpen(!isOpen);
 
-    return (
-        <nav className={styles.nav}>
-            <Link className={styles.title} href="/">
-                <Image src="/logo_transparent.png" alt="Pixel Bloom" height={44} width={58} />
-                PIXEL BLOOM
-            </Link>
+  return (
+    <nav className={styles.nav}>
+      <Link className={styles.title} href="/">
+        <Image
+          src="/logo_transparent.png"
+          alt="Pixel Bloom"
+          height={44}
+          width={58}
+        />
+        PIXEL BLOOM
+      </Link>
 
-            <div className={styles.burgerIcon} onClick={toggleMenu}>
-                <div className={`${styles.burgerLine} ${isOpen ? styles.openTop : ""}`}></div>
-                <div className={`${styles.burgerLine} ${isOpen ? styles.openMiddle : ""}`}></div>
-                <div className={`${styles.burgerLine} ${isOpen ? styles.openBottom : ""}`}></div>
-            </div>
+      <div className={styles.burgerIcon} onClick={toggleMenu}>
+        <div
+          className={`${styles.burgerLine} ${isOpen ? styles.openTop : ""}`}
+        ></div>
+        <div
+          className={`${styles.burgerLine} ${isOpen ? styles.openMiddle : ""}`}
+        ></div>
+        <div
+          className={`${styles.burgerLine} ${isOpen ? styles.openBottom : ""}`}
+        ></div>
+      </div>
 
-            <ul className={`${styles.menu} ${isOpen ? styles.menuOpen : styles.menuClosed}`}>
-                <Link className={styles.link} href="/projects"><li>Nos projets</li></Link>
-                <Link className={styles.link} href="/agency"><li>Notre Agence</li></Link>
-                <Link className={styles.link} href="/contact"><li>Contactez-nous</li></Link>
-            </ul>
-        </nav>
-    );
+      <ul
+        className={`${styles.menu} ${
+          isOpen ? styles.menuOpen : styles.menuClosed
+        }`}
+      >
+        <li>
+          <Link className={styles.link} href="/projects">
+            Nos projets
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/agency">
+            Notre Agence
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/contact">
+            Contactez-nous
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
