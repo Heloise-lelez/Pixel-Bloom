a7e88074cea702b909eed84c8d7a6cdd24b717f0
Héloïse Le Lez <lelez.heloise@gmail.com>
2025-01-30 15:48:24 +0100

    🩹 fix project images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["gs-expertise-two.vercel.app"],
+    domains: [
+      "gs-expertise-two.vercel.app",
+      "www.erawati.com",
+      "imag.malavida.com",
+      "www.tendancehotellerie.fr",
+    ],
   },
   /* config options here */
 };
